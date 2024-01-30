8dc27b121d8e3b1073fa78a0085a271c54430688
michał <51276321+michalzuch@users.noreply.github.com>
2024-01-30 19:36:40 +0100

    Tests - Add functional tests
    - update project data
    - add assertions for UI tests

--- Tests/Tests/Model/SampleData.swift
+++ Tests/Tests/Model/SampleData.swift
@@ -21,7 +21,39 @@ private let todoDescription =
 "5.0 Display status on the task list\n" +
 "Deadline: 7.12\n"
 
-private let unknownDescription =
+private let storeDescription =
+"3.0 Create Product and Category models in Core Data\n" +
+"3.5 3.5 Load Data on application startup (Fixtures)\n" +
+"4.0 Create a product list with a product description subpage in a new view (data from Core Data)\n" +
+"4.5 Add option to add product to bag (TabView)\n" +
+"5.0 Add option to ad more than one instance of the same product to bag\n" +
+"Deadline: ??.??\n"
+
+private let onlineStoreDescription =
+"3.0 Fetch products and categories from server application\n" +
+"3.5 Save data locally to Core Data\n" +
+"4.0 Fetch orders from server application (min 5 fields, including one relation)\n" +
+"4.5 Display products and categories on two separate lists\n" +
+"5.0 Add products from the mobile application\n" +
+"Deadline: ??.??\n"
+
+private let oAuthDescription =
+"3.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"3.5 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"4.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"4.5 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"5.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"Deadline: ??.??\n"
+
+private let paymentsDescription =
+"3.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"3.5 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"4.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"4.5 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"5.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+"Deadline: ??.??\n"
+
+private let testsDescription =
 "3.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
 "3.5 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
 "4.0 Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
@@ -32,8 +64,9 @@ private let unknownDescription =
 var sampleData: [Task] = [
     Task(title: "Calculator", description: calculatorDescription, image: "Calculator"),
     Task(title: "To Do", description: todoDescription, image: "To Do"),
-    Task(title: "Unknown", description: unknownDescription, image: "Unknown"),
-    Task(title: "Unknown", description: unknownDescription, image: "Unknown"),
-    Task(title: "Unknown", description: unknownDescription, image: "Unknown"),
-    Task(title: "Unknown", description: unknownDescription, image: "Unknown")
+    Task(title: "Store", description: storeDescription, image: "Store"),
+    Task(title: "Online Store", description: onlineStoreDescription, image: "Online Store"),
+    Task(title: "OAuth", description: oAuthDescription, image: "Unknown"),
+    Task(title: "Payments", description: paymentsDescription, image: "Unknown"),
+    Task(title: "Tests", description: testsDescription, image: "Unknown")
 ]
--- Tests/TestsUITests/TestsUITests.swift
+++ Tests/TestsUITests/TestsUITests.swift
@@ -27,7 +27,96 @@ final class TestsUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let title = app.staticTexts["Tasks"]
+        XCTAssertTrue(title.exists)
+
+        let calculatorLabel = app.staticTexts["Calculator"]
+        XCTAssertTrue(calculatorLabel.exists)
+        let calculatorImage = app.images["Calculator"]
+        XCTAssertTrue(calculatorImage.exists)
+        calculatorLabel.tap()
+        XCTAssertTrue(calculatorLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let toDoLabel = app.staticTexts["To Do"]
+        XCTAssertTrue(toDoLabel.exists)
+        let toDoImage = app.staticTexts["To Do"]
+        XCTAssertTrue(toDoImage.exists)
+        toDoLabel.tap()
+        XCTAssertTrue(toDoLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let storeLabel = app.staticTexts["Store"]
+        XCTAssertTrue(storeLabel.exists)
+        let storeImage = app.staticTexts["Store"]
+        XCTAssertTrue(storeImage.exists)
+        storeLabel.tap()
+        XCTAssertTrue(storeLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let onlineStoreLabel = app.staticTexts["Online Store"]
+        XCTAssertTrue(onlineStoreLabel.exists)
+        let onlineStoreImage = app.staticTexts["Online Store"]
+        XCTAssertTrue(onlineStoreImage.exists)
+        onlineStoreLabel.tap()
+        XCTAssertTrue(onlineStoreLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let oAuthLabel = app.staticTexts["OAuth"]
+        XCTAssertTrue(oAuthLabel.exists)
+        oAuthLabel.tap()
+        XCTAssertTrue(oAuthLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let paymentsLabel = app.staticTexts["Payments"]
+        XCTAssertTrue(paymentsLabel.exists)
+        paymentsLabel.tap()
+        XCTAssertTrue(paymentsLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let testsLabel = app.staticTexts["Tests"]
+        XCTAssertTrue(testsLabel.exists)
+        testsLabel.tap()
+        XCTAssertTrue(testsLabel.exists)
+        app.navigationBars.buttons["Tasks"].tap()
+        XCTAssertTrue(title.exists)
+
+        let unknownImage = app.images.matching(identifier: "Unknown")
+        XCTAssertEqual(unknownImage.count, 3)
+
+        let cellToDelete = app.cells.firstMatch
+        XCTAssertTrue(cellToDelete.exists)
+        XCTAssertFalse(app.buttons["Delete"].exists)
+        cellToDelete.swipeLeft()
+        let deleteButton = app.buttons["Delete"]
+        XCTAssertTrue(deleteButton.exists)
+        deleteButton.tap()
+        XCTAssertFalse(calculatorLabel.exists)
+
+        let statusCell = app.cells.firstMatch
+        XCTAssertTrue(statusCell.exists)
+        XCTAssertFalse(app.buttons["Done"].exists)
+        XCTAssertFalse(app.buttons["To Do"].exists)
+        statusCell.swipeRight()
+        var statusButton = app.buttons["Done"]
+        XCTAssertTrue(app.buttons["Done"].exists)
+        XCTAssertFalse(app.buttons["To Do"].exists)
+        statusButton.tap()
+        XCTAssertFalse(app.buttons["Done"].exists)
+        XCTAssertFalse(app.buttons["To Do"].exists)
+        statusCell.swipeRight()
+        statusButton = app.buttons["To Do"]
+        XCTAssertFalse(app.buttons["Done"].exists)
+        XCTAssertTrue(app.buttons["To Do"].exists)
+        statusButton.tap()
+        XCTAssertFalse(app.buttons["Done"].exists)
+        XCTAssertFalse(app.buttons["To Do"].exists)
     }
 
     func testLaunchPerformance() throws {
